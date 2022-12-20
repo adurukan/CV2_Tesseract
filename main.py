182dc76bf6a77c7d0681f8b9095b123ad3cd1442
Alpay <alpaysedatdurukan@gmail.com>
2022-12-20 15:23:47 +0000

    only lang support for eng and osd

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ arg_path = sys.argv[1]
 img_cv = cv2.imread(arg_path)
 
 img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-
+# print(pytesseract.get_languages(config=""))
 with open(arg_path.split(".")[0] + ".txt", "w") as f:
-    f.write(pytesseract.image_to_string(img_rgb))
+    f.write(pytesseract.image_to_string(img_rgb, lang="eng"))
     f.close()
